ed9556df9350410bfd12bbb8bf48a3557a3018ec
suruchisinha <suruchisinha.10@gmail.com>
2017-08-17 18:49:56 +0530

    modules all file

---
+++ modules/custom/resume/tests/src/Unit/DataTest.php
@@ -0,0 +1,21 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+require './Unit/CsvFileIterator.php';
+
+class DataTest extends TestCase
+{
+    /**
+     * @dataProvider additionProvider
+     */
+    public function testAdd($a, $b, $expected)
+    {
+        $this->assertEquals($expected, $a + $b);
+    }
+
+    public function additionProvider()
+    {
+        return new CsvFileIterator('data.csv');
+    }
+}
+?>
